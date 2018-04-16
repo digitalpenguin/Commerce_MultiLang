61b93ce13f902278ef0778ed49fc3afddc26163c
Murray <murray@digitalpenguin.hk>
2018-04-16 19:24:21 +0800

    Multi language tabs working

--- core/components/commercemultilang/processors/mgr/product/create.class.php
+++ core/components/commercemultilang/processors/mgr/product/create.class.php
@@ -9,6 +9,32 @@ class CommerceMultiLangProductCreateProcessor extends modObjectCreateProcessor {
     public $classKey = 'CommerceMultiLangProduct';
     public $languageTopics = array('commercemultilang:default');
     public $objectType = 'commercemultilang.product';
+    protected $langKeys = array();
+
+    protected function getLanguages() {
+        $c = $this->modx->newQuery('modContext');
+        $c->leftJoin('modContextSetting','ContextSettings','modContext.key=ContextSettings.context_key');
+        $c->select('modContext.key,ContextSettings.key as setting_key,ContextSettings.value as lang_key');
+        $c->where(array(
+            'modContext.key:!=' => 'mgr',
+            'AND:ContextSettings.key:=' => 'cultureKey'
+        ));
+        $contexts = $this->modx->getCollection('modContext',$c);
+        foreach ($contexts as $context) {
+            $contextArray = $context->toArray();
+            //$this->modx->log(1,print_r($contextArray,true));
+            $lang = array();
+            $lang['context_key'] = $contextArray['key'];
+            $lang['lang_key'] = $contextArray['lang_key'];
+            $lang['name'] = $contextArray['name'];
+            array_push($this->langKeys,$lang);
+        }
+    }
+
+    public function initialize() {
+        $this->getLanguages();
+        return parent::initialize();
+    }
 
     public function beforeSave() {
         $name = $this->getProperty('name');
@@ -21,14 +47,15 @@ class CommerceMultiLangProductCreateProcessor extends modObjectCreateProcessor {
     }
 
     public function afterSave() {
-        $productLang = $this->modx->newObject('CommerceMultiLangProductLanguage');
-        $productLang->set('product_id',$this->object->get('id'));
-        $productLang->set('name',$this->object->get('name'));
-        $productLang->set('lang_key',$this->modx->getOption('commercemultilang.default_lang'));
-        $productLang->set('description',$this->object->get('description'));
-        $this->modx->log(1,'This is the description:'.$this->object->get('description'));
-        $productLang->save();
-
+        foreach ($this->langKeys as $langKey) {
+            $productLang = $this->modx->newObject('CommerceMultiLangProductLanguage');
+            $productLang->set('product_id', $this->object->get('id'));
+            $productLang->set('name', $this->object->get('name'));
+            $productLang->set('lang_key', $langKey['lang_key']);
+            $productLang->set('description', $this->object->get('description'));
+            $this->modx->log(1, 'This is the description:' . $this->object->get('description'));
+            $productLang->save();
+        }
         $productData = $this->modx->newObject('CommerceMultiLangProductData');
         $productData->set('product_id',$this->object->get('id'));
         foreach($this->getProperties() as $key => $value) {
