f5ec51834317f67ae4be971614ddf246edd2c149
Murray <murray@digitalpenguin.hk>
2018-05-18 08:43:09 +0800

    Testing github docs

--- core/components/commercemultilang/processors/mgr/product/image/update.class.php
+++ core/components/commercemultilang/processors/mgr/product/image/update.class.php
@@ -10,5 +10,17 @@ class CommerceMultiLangProductImageUpdateProcessor extends modObjectUpdateProces
     public $classKey = 'CommerceMultiLangProductImage';
     public $languageTopics = array('commercemultilang:default');
     public $objectType = 'commercemultilang.productimage';
+
+    public function afterSave() {
+        // Grabs related language table
+        $imageLanguages = $this->modx->getCollection('CommerceMultiLangProductLanguage',[
+            'product_id'    => $this->object->get('id')
+        ]);
+        foreach ($imageLanguages as $language) {
+            $langKey = $language->get('lang_key');
+        }
+        return parent::afterSave();
+    }
+
 }
 return 'CommerceMultiLangProductImageUpdateProcessor';
